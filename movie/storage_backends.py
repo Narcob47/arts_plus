0c72ded799b76949c04d6f6822afb32595fb18ec
Narco Shilukukwa <137599138+Narcob47@users.noreply.github.com>
2025-02-04 17:49:47 +0200

    Update storage_backends.py

--- movie/storage_backends.py
+++ movie/storage_backends.py
@@ -1,6 +1,7 @@
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    # account_name = 'geco2studios'  # Must be replaced by your <storage_account_name>
-    # azure_container = 'studios'
+    account_name = 'geco2studios'  # Must be replaced by your <storage_account_name>
+    AZURE_ACCOUNT_KEY = "2VqmDrYoffc1YwvH1+4aSTfbhoPf/YLJuJGpM0lkIJ/F5nzkC7AS8VFOicN/lXUU9zJRs12RLSKJ+AStgxnCVA=="
+    azure_container = 'studios'
     expiration_secs = None
